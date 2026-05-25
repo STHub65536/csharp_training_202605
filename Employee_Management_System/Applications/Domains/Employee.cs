3f7b3d31c4d2b0d87078ed152ef94b1355e613ab
STHub65536 <2026opnewemp006@fullness.email>
2026-05-25 12:21:11 +0900

    実装:Employee:CheckCorrectAge(適切な年齢かどうかのチェック)

--- Employee_Management_System/Applications/Domains/Employee.cs
+++ Employee_Management_System/Applications/Domains/Employee.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection.Metadata;
 using System.Threading.Tasks;
 
 namespace Employee_Management_System.Applications.Domains;
@@ -12,6 +13,9 @@ public class Employee
     public string MailAddress { get; }
     public int? DeptNo { get; }
     public Department? Dept { get; }
+
+    public static readonly int MAX_AGE = 100;
+    public static readonly int MIN_AGE = 15;
 
     public Employee(int? EmpNo, string EmpName, DateOnly Birthday, string MailAddress, int? DeptNo, Department? Dept)
     {
@@ -30,6 +34,35 @@ public class Employee
         this.Birthday = Birthday;
         this.MailAddress = MailAddress;
         this.DeptNo = DeptNo;
+    }
+
+    public Employee(string EmpName, DateOnly Birthday, string MailAddress, int? DeptNo)
+    {
+        this.EmpNo = EmpNo;
+        this.EmpName = EmpName;
+        this.Birthday = Birthday;
+        this.MailAddress = MailAddress;
+        this.DeptNo = DeptNo;
+    }
+
+    public bool CheckCorrectAge()
+    {
+        DateOnly currentDate = DateOnly.FromDateTime(DateTime.Now);
+
+        int age = currentDate.Year - Birthday.Year;
+        if(Birthday.Month > currentDate.Month || (Birthday.Month == currentDate.Month && Birthday.Day > currentDate.Day))
+        {
+            age--;
+        }
+
+        if(MIN_AGE <= age && age <= MAX_AGE)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
     }
 
     public override bool Equals(object? obj)
